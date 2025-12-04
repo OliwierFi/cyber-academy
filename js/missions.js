9e6044fc8f4305d0aa0c9d540b550f85320ab842
OliwierFi <fijalkowskioliwier@gmail.com>
2025-12-04 01:03:22 +0100

    Create missions.js

---
+++ js/missions.js
@@ -0,0 +1,6 @@
+export const MISSIONS = [
+  {id:'m1', title:'Ukończ 1 lekcję', xp:150},
+  {id:'m2', title:'Zrób 1 quiz (łatwy)', xp:100},
+  {id:'m3', title:'Przerób 20 fiszek', xp:200},
+  {id:'m4', title:'Zaloguj się przez Google', xp:50}
+];
